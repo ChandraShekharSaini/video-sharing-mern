71ec6c67cd08f8b008d4c24c21259960209a39ce
ChandraShekharSaini <chandrashekharsaini322@gmail.com>
2025-06-27 20:47:06 +0530

    Created MongoDb schema

---
+++ backend/src/model/userSchema.model.ts
@@ -0,0 +1,48 @@
+import { MongoOIDCError } from "mongodb";
+import mongoose, { Schema, Document, Model } from "mongoose";
+
+export interface IUser extends Document {
+    username?: string,
+    email: string,
+    password: string,
+    token: string,
+    uploadCount: number,
+    downloadCount: number,
+    createdAt: Date,
+    updatedAt: Date
+}
+
+const userSchema = new Schema({
+
+    username: {
+        type: String,
+    },
+
+    email: {
+        type: String,
+        require: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+
+    token: {
+        type: String
+    },
+
+    uploadCount: {
+        type: Number,
+        default: 0
+    },
+
+    downloadCount: {
+        type: Number,
+        default: 0
+    }
+}, { timestamps: true })
+
+const User: Model<IUser> = mongoose.model<IUser>('User', userSchema)
+export default User
+
